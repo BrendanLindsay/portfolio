28bfd1e3418f9dac68cfe52c297e263aecd2611b
Brendan Lindsay <brendan.g.lindsay@gmail.com>
2017-04-12 18:03:13 -0400

    Fixed the form!
    Tried some new code, the error was due to the absence of "isset", and
    maybe a phantom bracket...

--- php/form.php
+++ php/form.php
@@ -1,19 +1,17 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
-    $from = 'From: Portfolio Website';
-    $to = 'brendan.g.lindsay@gmail.com';
-    $subject = 'New message from portfolio!';
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+        $from = 'Portfolio Website Inbox';
+        $to = 'brendan.g.lindsay@gmail.com';
+        $subject = 'New message from portfolio site!';
 
-    $body = "From: $name\n E-Mail: $email\n Phone: $phone\n Message:\n $message";
+        $body = "From: $name\n E-Mail: $email\n Phone: $phone\n Message:\n $message";
 
-    if ($_POST['submit']) {
         if (mail ($to, $subject, $body, $from)) {
-            echo "<p>Thanks for the message, i'll talk to you soon!</p>";
-        } else {
-            echo "<p>Huh-oh, that didn't work..try again for me?</p>";
-        }
+	} else {
+	}
     }
     ?>
